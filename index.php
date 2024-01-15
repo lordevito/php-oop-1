2342b18ccd4657239a328505799fadc9124119d1
lordevito <lollo.devito@hotmail.com>
2024-01-15 17:29:57 +0100

    feat: metodi e costruttore

--- index.php
+++ index.php
@@ -5,5 +5,40 @@ class Movie {
   public $description;
   public $year;
   public $genres;
-}
 
+//COSTRUTTORE
+
+public function __construct(string $title, string $description, int $year, array $genres = [])
+  {
+    $this->title = $title;
+    $this->description = $description;
+    $this->year = $year;
+    $this->genres = $genres;
+  }
+
+//METODI
+
+  //Controlla se l'anno e restituisce una stringa
+
+  public function checkYear() {
+    if($this->year < 2000) {
+      echo 'inizia ad essere d\'altri tempi';
+    };
+  }
+
+  //Stampa i generi
+  private function printGenres(){
+    foreach($this->genres as $genre){
+      echo $genre .' ';
+    }
+  }
+
+  //Controlla se l'array dei generi è pieno
+  public function hasGenres(){
+    if(empty($this->genres)){
+      $this->genres[0] = 'nessun genere definito';
+    }
+    $this->printGenres();
+  }
+
+}
