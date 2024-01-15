a0f1b2d38da7f976507bfa27c7aa1b23770ea66f
lordevito <lollo.devito@hotmail.com>
2024-01-15 18:49:05 +0100

    feat: classi e oggetti

--- index.php
+++ index.php
@@ -42,3 +42,67 @@ public function __construct(string $title, string $description, int $year, array
   }
 
 }
+
+//crea instanze per film da classe Movie
+$lord = new Movie('Il Signore degli Anelli - La Compagnia dell\'Anello', 'Un giovane hobbit e un variegato gruppo, composto da umani, un nano, un elfo e altri hobbit, partono per un delicata missione, guidati dal potente mago Gandalf. Devono distruggere un anello magico e sconfiggere così il malvagio Sauron.', 2002, ['Azione','Avventura','Fantascienza']);
+
+echo 'Oggetto Film';
+print '<br>';
+
+print_r($lord);
+print '<br><br>';
+
+echo 'Elenco attributi';
+print '<br>';
+
+//Controlla se ci sono generi
+echo $lord->title;
+print '<br>';
+echo $lord->year;
+print '<br>';
+//stampa i generi
+$lord->hasGenres();
+
+print '<br>'.'<br>';
+
+echo $lord->description;
+
+print '<br>'.'<br>';
+
+echo 'Metodi';
+echo '<br>';
+print $lord->checkYear();
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+
+$matrix = new Movie('Matrix', 'Esistono due realtà: una è l\'esistenza che conduciamo ogni giorno, l\'altra è nascosta. Neo vuole scoprire la verità su Matrix, mondo virtuale elaborato al computer creato per tenere sotto controllo le persone. Morpheus potrebbe aiutarlo.', 1999, ['Azione', 'Fantastico']);
+
+echo 'Oggetto Film';
+print '<br>';
+
+print_r($matrix);
+print '<br><br>';
+
+echo 'Elenco attributi';
+print '<br>';
+
+//Controlla se ci sono generi
+echo $matrix->title;
+print '<br>';
+echo $matrix->year;
+print '<br>';
+//stampa i generi
+$matrix->hasGenres();
+
+print '<br>'.'<br>';
+
+echo $matrix->description;
+
+print '<br>'.'<br>';
+
+echo 'Metodi';
+echo '<br>';
+print $matrix->checkYear();
